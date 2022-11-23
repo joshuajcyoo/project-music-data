9e2c741be63de353893be7396d55e24126c82c55
joshuajcyoo <jjyoo@usc.edu>
2022-11-23 14:38:26 -0800

    11/23: Initial Setup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,78 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css";
+import { createBrowserRouter, RouterProvider, redirect } from "react-router-dom";
+import { toast } from 'react-toastify';
+
+import Root from './Routes/Root';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/stats",
+        element: <SpotifyStats />,
+        loader({ }) {
+          return;
+        },
+        children: [
+          {
+            path: "/stats/:id/comments",
+            element: <Comments />
+          }
+        ]
+      },
+      {
+        path: "/scores",
+        element: <Scores />,
+        loader ({}) {
+          return;
+        },
+        children: [
+          {
+            path: "/scores/:id/comments",
+            element: <Comments />
+          },
+          {
+            path: "/scores/:album",
+            element: <Album />,
+            loader ({}) {
+              return;
+            },
+            children: [
+              {
+                path: "/scores/:album/:song",
+                element: <Song />,
+                loader ({}) {
+                  return; 
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        path: "/admin",
+        element: <Admin />,
+        loader ({}) {
+          return;
+        }
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
